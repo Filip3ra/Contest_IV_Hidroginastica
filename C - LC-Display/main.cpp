f4f837e7727d58da40596c0c2b047313c2c6893f
Filip3ra <filipimacielrj@gmail.com>
2022-03-10 06:37:07 -0300

    progresso inicial na questao

--- C - LC-Display/main.cpp
+++ C - LC-Display/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int conta_digitos(int num) {
+   return int(log10(num) + 1);
+}
+
 int main(int argc, char const *argv[]){
 
+    int s, n;         // 'n' é o número e 's' é o tamanho dele
+    int qtd_digitos;  //
+
+    while (cin >> s >> n){
+
+        if(s == 0 && n == 0)
+            break;
+
+        // decubro quantos dígitos tem meu número
+        qtd_digitos = conta_digitos(n);
+
+        // crio uma matriz com tamanho adequado as regras
+
+        
+        cout << s << " " << n << "qtd digitos = " << qtd_digitos << endl;
+    }
     
 
+    
     return 0;
 }
+
+
