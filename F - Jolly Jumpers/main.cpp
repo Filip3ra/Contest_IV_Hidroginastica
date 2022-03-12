0d8ac88c511405497bd0313e9a5d3d2d3a5f5b6b
“Filip3ra” <filipimacielrj@gmail.com>
2022-03-12 07:50:51 -0300

    questao F e I aceitas

--- F - Jolly Jumpers/main.cpp
+++ F - Jolly Jumpers/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -6,37 +7,38 @@ int main(int argc, char const *argv[]){
 
     int i, j, n, diferenca, sequencia[3001], vet_bits[3001];
     
-    cin >> n;
+    while( !(cin >> n).eof() ){
 
-    for(i = 0; i<n; ++i)
-        vet_bits[i] = 0;
+        for(i = 0; i<n; ++i)
+            vet_bits[i] = 0;
 
-    for(i = 0; i<n; ++i)
-        cin >> sequencia[i];
+        for(i = 0; i<n; ++i)
+            cin >> sequencia[i];
 
-    bool condicao = false;
-    for(i = 0; i<n-1; ++i){
-        diferenca = abs(sequencia[i] - sequencia[i+1]);
-        if(diferenca > n-1){
-            cout << "Not jolly" << endl;
-            condicao = true;
-            break;
-        }
-        else
-            vet_bits[diferenca] = 1;
-    }
-
-    if(!condicao){
-        for(i = 1; i<n-1; ++i){
-            if(vet_bits[i] != 1){
+        bool condicao = false;
+        for(i = 0; i<n-1; ++i){
+            diferenca = abs(sequencia[i] - sequencia[i+1]);
+            if( diferenca < 1 || diferenca > n-1 || vet_bits[diferenca] == 1){
                 cout << "Not jolly" << endl;
                 condicao = true;
                 break;
             }
+            else
+                vet_bits[diferenca] = 1;
         }
-        if(!condicao)
-            cout << "Jolly" << endl;
-    }
 
+        if(!condicao){
+            for(i = 1; i<n-1; ++i){
+                if(vet_bits[i] != 1){
+                    cout << "Not jolly" << endl;
+                    condicao = true;
+                    break;
+                }
+            }
+            if(!condicao)
+                cout << "Jolly" << endl;
+        }
+    }
     return 0;
 }
+
