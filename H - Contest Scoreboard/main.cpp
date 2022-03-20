05e39c273a32685b66b5549ab8f69fc26f179cb8
Filip3ra <filipimacielrj@gmail.com>
2022-03-20 10:45:02 -0300

    erro na leitura dos inputs

--- H - Contest Scoreboard/main.cpp
+++ H - Contest Scoreboard/main.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
-    
+    int n, contador = 0;
+    int c, p, t;
+    char L;
+
+    cin >> n;
+    cout << n << endl;
+
+
+    while( true ){
+        
+        if((cin >> c >> p >> t >> L).eof())
+            break;
+        else if((cin >> c >> p >> t >> L) == n){
+            
+        }
+
+
+
+        cout << c << " " << p << " " << t << " " << L << endl; 
+
+        contador++;
+    }
 
     return 0;
 }
